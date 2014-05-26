6ee24c16c09088fab7cea9665d970dc9dc3e4818
David Zhong <dzhong111@gmail.com>
2014-05-26 23:14:04 +1000

    Renamed id column

--- app/views/atc/form.php
+++ app/views/atc/form.php
@@ -1,4 +1,4 @@
 <h1>ATC Information</h1>
 <p>Stuff goes here...</p>
-<p><?=$user->uid?> <?=$user->name?> <?=$user->email?></p>
+<p><?=$user->id?> <?=$user->name?> <?=$user->email?></p>
 <pre><?php var_dump($user->rs); ?></pre>
