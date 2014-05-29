b15608f9d11824a7f001f39c8b32240758aa35c3
David Zhong <dzhong111@gmail.com>
2014-05-30 04:14:53 +1000

    Added footer and various minor visual changes

--- app/views/layout.php
+++ app/views/layout.php
@@ -20,7 +20,7 @@ function menu_active($item) {
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/realops14.css">
+    <link rel="stylesheet" href="/css/realops.css">
 <?php if ($menu == 'index') { ?>
     <link rel="stylesheet" href="/css/header.css">
 <?php } ?>
@@ -32,59 +32,60 @@ function menu_active($item) {
     <![endif]-->
   </head>
   <body>
-    <header>
-      <nav class="navbar navbar-default navbar-static-top navbar-inverse" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#realops-navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-          </div><!-- /.navbar-header -->
-          <div class="collapse navbar-collapse" id="realops-navbar">
-            <ul class="nav navbar-nav navbar-right">
-              <li<?php menu_active('index') ?>><a href="<?=Route::link('index')?>">Home</a></li>
-              <li<?php menu_active('sked') ?>><a href="<?=Route::link('schedule')?>">Book a Flight</a></li>
-              <li<?php menu_active('pilots') ?>><a href="<?=Route::link('pilots')?>">Pilot Briefing</a></li>
-              <li<?php menu_active('charts') ?>><a href="<?=Route::link('charts')?>">Scenery and Charts</a></li>
-              <li<?php menu_active('atc') ?>><a href="<?=Route::link('atc')?>">ATC Information</a></li>
-              <li<?php menu_active('contact') ?>><a href="<?=Route::link('contact')?>">Contact Us</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container -->
-      </nav>
-<?php if ($menu == 'index') { ?>
-    <div id="realops-calltoaction">
-      <a href="<?=Route::link('schedule')?>" class="btn btn-primary btn-lg">Book your flight now!</a>
-    </div>
-    <div id="realops-background-shade"></div>
-    <div id="realops-logo"></div>
-<?php } ?>
-    </header>
-    <div class="container" id="realops-container">
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="panel panel-default" id="realops-login-panel">
-            <div class="panel-heading">User Control Panel</div>
-            <div class="panel-body">
-              <?php include(APP_PATH . "inc/usercontrol.php") ?>
-            </div><!-- /.panel-body -->
-          </div><!-- /#realops-login-panel -->
-          <div class="panel panel-default hidden-xs">
-            <div class="panel-heading">Quick links</div>
-            <div class="panel-body">
-              <ul class="list-unstyled">
-                <li><a href="<?=Route::link('schedules')?>">Flight schedules</a></li>
-                <li><a href="<?=Route::link('pilots')?>">Pilot briefing</a></li>
-                <li><a href="#" target="_blank">Discussion forum</a></li>
-                <li><a href="http://www.vatsim.net/fp/" target="_blank">Flight plan submission</a></li>
+    <div class="wrapper">
+      <header>
+        <nav class="navbar navbar-default navbar-static-top navbar-inverse" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#realops-navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div><!-- /.navbar-header -->
+            <div class="collapse navbar-collapse" id="realops-navbar">
+              <ul class="nav navbar-nav navbar-right">
+                <li<?php menu_active('index') ?>><a href="<?=Route::link('index')?>">Home</a></li>
+                <li<?php menu_active('sked') ?>><a href="<?=Route::link('schedule')?>">Book a Flight</a></li>
+                <li<?php menu_active('pilots') ?>><a href="<?=Route::link('pilots')?>">Pilot Briefing</a></li>
+                <li<?php menu_active('charts') ?>><a href="<?=Route::link('charts')?>">Scenery and Charts</a></li>
+                <li<?php menu_active('atc') ?>><a href="<?=Route::link('atc')?>">ATC Information</a></li>
+                <li<?php menu_active('contact') ?>><a href="<?=Route::link('contact')?>">Contact Us</a></li>
               </ul>
-            </div><!-- /.panel-body -->
-          </div><!-- /.panel-default -->
-        </div><!-- /.col-sm-3 -->
-        <div class="col-sm-8 panel panel-default" id="realops-body">
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container -->
+        </nav>
+  <?php if ($menu == 'index') { ?>
+      <div id="realops-calltoaction">
+        <a href="<?=Route::link('schedule')?>" class="btn btn-primary btn-lg">Book your flight now!</a>
+      </div>
+      <div id="realops-background-shade"></div>
+      <div id="realops-logo"></div>
+  <?php } ?>
+      </header>
+      <div class="container" id="realops-container">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="panel panel-default" id="realops-login-panel">
+              <div class="panel-heading">User Control Panel</div>
+              <div class="panel-body">
+                <?php include(APP_PATH . "inc/usercontrol.php") ?>
+              </div><!-- /.panel-body -->
+            </div><!-- /#realops-login-panel -->
+            <div class="panel panel-default hidden-xs">
+              <div class="panel-heading">Quick links</div>
+              <div class="panel-body">
+                <ul class="list-unstyled">
+                  <li><a href="<?=Route::link('schedules')?>">Flight schedules</a></li>
+                  <li><a href="<?=Route::link('pilots')?>">Pilot briefing</a></li>
+                  <li><a href="#" target="_blank">Discussion forum</a></li>
+                  <li><a href="http://www.vatsim.net/fp/" target="_blank">Flight plan submission</a></li>
+                </ul>
+              </div><!-- /.panel-body -->
+            </div><!-- /.panel-default -->
+          </div><!-- /.col-sm-3 -->
+          <article class="col-sm-8 panel panel-default" id="realops-body">
 <?php
 if (isset($body)) {
   if (is_array($body)) {
@@ -96,10 +97,13 @@ if (isset($body)) {
   }
 }
 ?>
-        </div><!-- /.col-sm-8 -->
-      </div><!-- /.row -->
-    </div><!-- /.container -->
-
+          </article>
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </div><!-- /.wrapper -->
+    <footer>
+      <p>Website by &copy; David Zhong 2014<br />Photographs curtesy of Sydney Airport Corportation Limited</p>
+    </footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
