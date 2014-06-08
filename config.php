ae0509ca4da199066e5f2878b1e841b895d46642
David Zhong <dzhong111@gmail.com>
2014-06-09 01:21:57 +1000

    Added configuration file

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+//===============================================
+// Debug
+//===============================================
+define('DEBUG', true);
+
+//===============================================
+// Website options
+//===============================================
+define('APP_PATH','app/'); //with trailing slash
+define('WEB_FOLDER',''); //with leading slash
+define('SITENAME', 'RealOps Sydney 2014 - VATSIM Australia Pacific');
+
+//===============================================
+// Database
+//===============================================
+define('SQL_HOST', '');
+define('SQL_DATABASE', '');
+define('SQL_USER', '');
+define('SQL_PASSWORD', '');
--- index.php
+++ index.php
@@ -1,79 +1,51 @@
 <?php
-//===============================================
+// Load config
+require("config.php");
+
 // Debug
-//===============================================
-ini_set('display_errors','On');
-error_reporting(E_ALL);
-//ini_set('display_errors','Off');
-//error_reporting(E_NONE);
+if (DEBUG) {
+  ini_set('display_errors','On');
+  error_reporting(E_ALL);
+} else {
+  ini_set('display_errors','Off');
+  error_reporting(E_NONE);
+}
 
-//===============================================
-// Madatory KISSMVC Settings (please configure)
-//===============================================
-define('APP_PATH','app/'); //with trailing slash pls
-define('WEB_FOLDER',''); //with trailing slash pls
-
-//===============================================
-// Other Settings
-//===============================================
+// Website options
 $GLOBALS['sitename']='RealOps Sydney 2014 - VATSIM Australia Pacific';
 
-//===============================================
 // Includes
-//===============================================
 require('kissmvc.php');
 require(APP_PATH . 'route.php');
+require(APP_PATH . 'auth.php');
 
-//===============================================
 // Session
-//===============================================
-
+$cookie_path = APP_PATH ? APP_PATH : '/';
+session_set_cookie_params(0, $cookie_path, $_SERVER['HTTP_HOST'], false, true);
 session_start();
 
-//===============================================
-// Uncaught Exception Handling
-//===============================================s
-/*
-set_exception_handler('uncaught_exception_handler');
-
-function uncaught_exception_handler($e) {
-  ob_end_clean(); //dump out remaining buffered text
-  $vars['message']=$e;
-  die(View::do_fetch(APP_PATH.'errors/exception_uncaught.php',$vars));
-}
-
-function custom_error($msg='') {
-  $vars['msg']=$msg;
-  die(View::do_fetch(APP_PATH.'errors/custom_error.php',$vars));
-}
-*/
-
-//===============================================
 // Database
-//===============================================
 function getdbh() {
   if (!isset($GLOBALS['dbh']))
     try {
-      $GLOBALS['dbh'] = new PDO('mysql:host=localhost;dbname=realops', 'root', '');
+      $GLOBALS['dbh'] = new PDO('mysql:host=' . SQL_HOST . ';dbname=' .
+                                SQL_DATABASE, SQL_USER, SQL_PASSWORD);
     } catch (PDOException $e) {
       die('Connection failed: '.$e->getMessage());
     }
   return $GLOBALS['dbh'];
 }
 
-//===============================================
-// Autoloading for Business Classes
-//===============================================
-// Assumes Model Classes start with capital letters and Helpers start with lower case letters
+// Autoload business classes
+// Assumes Model Classes start with capital letters and helpers start
+// with lower case letters
 function __autoload($classname) {
   $a=$classname[0];
   if ($a >= 'A' && $a <='Z')
-    require_once(APP_PATH.'models/'.$classname.'.php');
+    require_once(APP_PATH . 'models/' . $classname . '.php');
   else
-    require_once(APP_PATH.'helpers/'.$classname.'.php');  
+    require_once(APP_PATH . 'helpers/' . $classname . '.php');  
 }
 
-//===============================================
-// Start the controller
-//===============================================
+// Start controller
 $controller = new Controller(APP_PATH.'controllers/',WEB_FOLDER);
