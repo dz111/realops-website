0464f60eb1c499758e6b5f3ecf6ed2ce5c0764ea
David Zhong <dzhong111@gmail.com>
2014-07-01 02:17:26 +1000

    Added pilot info pages

--- app/views/main/charts.php
+++ app/views/main/charts.php
@@ -1,2 +1,23 @@
 <h1>Scenery and Charts</h1>
-<p>Watch this space for more information...</p>
+<h3>Aerodrome charts</h3>
+<p>Charts for airports in Australia may be found at <a href="http://www.airservicesaustralia.com/aip/current/dap/AeroProcChartsTOC.htm">the Airservices Australia website</a>.</p>
+<h3>Scenery</h3>
+<p><strong>Flight Simulator 2004</strong>:</p>
+<ul>
+  <li><a href="http://aussiex.org/forum/index.php?/files/file/3569-yssy-sydney-airport-update-25/">Sydney Airport Update 2.5</a> by Robert Fluke - this is an update to the Sydney Airport scenery found in the famous Vista Australis scenery package.</li>
+  <li><a href="http://aussiex.org/forum/index.php?/files/file/505-ybbn-brisbane-international-airport-lite/">Brisbane Airport Lite</a> by Vista Australis</li>
+  <li><a href="http://aussiex.org/forum/index.php?/files/file/502-voz-18-complete/">Vista Australis 1.8</a> landclass and airports package</li>
+  <li><a href="http://aussiex.org/forum/index.php?/files/file/2650-fs9-andy-weirs-mesh-complete/">90m Australian mesh</a> by Andy Weir</li>
+</ul>
+<p><strong>Flight Simulator X</strong>:</p>
+<ul>
+  <li><a href="https://www.dropbox.com/s/itxiz7g9b2b6yfy/Sydney%20YSSY%20%5BVersion%201.1%5D.zip">AFCAD + photoreal base</a> with AFCAD by Ben Ohene and photoreal base by Zach Biesse-Fitton</li>
+  <li><a href="http://fullterrain.com/product_australia.html">FTX AU</a> by Orbx [PAYWARE] - landclass</li>
+  <li><a href="http://fullterrain.com/product_ybbn.html">FTX AU YBBN</a> by Orbx [PAYWARE]</li>
+  <li><a href="http://fullterrain.com/product_ymml2.html">FTX AU YMML</a> by Orbx [PAYWARE]</li>
+  <li><a href="https://docs.google.com/file/d/0By8a-7jAShVrN28xX2k2UHVOTW8/edit">Holger Mesh</a> by Holger Sandmann and distributed by Orbx [FREEWARE] - 76m Australian mesh</li>
+</ul>
+<p><strong>X-Plane 10</strong>:</p>
+<ul>
+  <li><a href="http://isdg.com.au/scenery-packages/yssy-sydney-international/">Sydney Photo Scenery version 1.3</a> by Chris Kawchuk and ISDG</li>
+</ul>
--- app/views/main/pilots.php
+++ app/views/main/pilots.php
@@ -1,2 +1,80 @@
 <h1>Pilot Briefing</h1>
-<p>Watch this space for more information...</p>
+<h3>Frequency management</h3>
+<p>Domestic aircraft should change to the ground or tower frequency automatically. The exception to this is aircraft vacating Runway 16L/34R who should remain on the tower frequency until instructed to change (this will happen on Taxiway Lima.</p>
+<p>Arriving aircraft will be transferred to Sydney Director when on downwind or on a 20 mile final. The main frequency will be 126.10 MHz, but in periods of heavier traffic, Director East may be activated to handle Runway 16L/34R arrivals on 125.30 MHz.</p>
+<table class="table">
+  <thead>
+    <tr>
+      <th>Position</th>
+      <th>Callsign</th>
+      <th>Frequency</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Airways Clearance Delivery</td>
+      <td>Sydney Delivery</td>
+      <td>133.80</td>
+    </tr>
+    <tr>
+      <td>SMC West</td>
+      <td>Sydney Ground</td>
+      <td>126.50</td>
+    </tr>
+    <tr>
+      <td>SMC East</td>
+      <td>Sydney Ground</td>
+      <td>121.70</td>
+    </tr>
+    <tr>
+      <td>Tower West</td>
+      <td>Sydney Tower</td>
+      <td>120.50</td>
+    </tr>
+    <tr>
+      <td>Tower East</td>
+      <td>Sydney Tower</td>
+      <td>124.70</td>
+    </tr>
+    <tr>
+      <td>Approach North</td>
+      <td>Sydney Approach</td>
+      <td>124.70</td>
+    </tr>
+    <tr>
+      <td>Approach South</td>
+      <td>Sydney Approach</td>
+      <td>123.80</td>
+    </tr>
+    <tr>
+      <td>Departures</td>
+      <td>Sydney Departures</td>
+      <td>123.00</td>
+    </tr>
+    <tr>
+      <td>Director West</td>
+      <td>Sydney Director</td>
+      <td>126.10</td>
+    </tr>
+    <tr>
+      <td>Director East</td>
+      <td>Sydney Director</td>
+      <td>125.30</td>
+    </tr>
+  </tbody>
+</table>
+<h3>Independent visual approaches</h3>
+<p>In visual meteorological conditions (VMC), it is likely that independent visual approaches (IVA) will be conducted to the parallel runways. You will know when IVAs are in use because the ATIS will advise you to "expect independent visual approach" and Sydney Director will be using the phraseology "cleared independent visual approach".</p>
+<p>When IVAs are in use, it is important that you report visual (land or water in sight and you are able to maintain visual flight) or runway-in-sight as soon as possible. This will allow controllers to plan separation and spacing accordingly.</p>
+<p>If you are cleared for an IVA, you should endeavour to track the runway extended centreline accurately as there may be aircraft on the adjacent centreline who may be on a different Director/Tower frequency.</p>
+<h3>Departure clearance</h3>
+<p>During periods of heavy departure traffic, airways clearance delivery will be available on 133.80 MHz. Otherwise, you will be able to obtain your clearance from the ground controller responsible for your side of the airport.</p>
+<h3>Ground movements</h3>
+<p>Clearance is required prior to pushback onto any taxiway. These can be identified on the Aerodrome Chart as any labelled path.</p>
+<p>Remember that you MUST <strong>obtain a clearance prior to entering any runway</strong>, including runways that are not listed in the ATIS. Any runway may be used at the discretion of ATC for ad-hoc operations, so make sure that you have received a clearance. If you are unsure, please contact ATC.</p>
+<p>When vacating the runway after landing, contact ground and advise them of your preferred parking position. The following image is a guide to the domestic terminal:</p>
+<img src="/img/domestic.png">
+<p>The following is a guide to the area of responsibility for each of the aerodrome control positions. Note that certain positions will be merged from time to time.</p>
+<img src="/img/yssy_aor.png">
+<h3>Extra fuel for traffic delays</h3>
+<p>We don't anticipate that there will be many traffic delays at this year's event, however, a NOTAM will be published 24 hours prior to the event with guidelines. This NOTAM will be made available on this website.</p>
