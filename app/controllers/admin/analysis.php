61e5e91410c68db3dd6c85b2afcce6205be5b5bc
David Zhong <dzhong111@gmail.com>
2014-07-02 18:42:41 +1000

    Add hourly analysis

---
+++ app/controllers/admin/analysis.php
@@ -0,0 +1,44 @@
+<?php
+function _analysis() {
+  $data = array("counts" => array("Sydney" => countairport('YSSY'),
+                                  "Brisbane" => countairport('YBBN'),
+                                  "Melbourne" => countairport('YMML')));
+  View::auto($data);
+}
+
+function countairport($port) {
+  $deps = Flight::where('adep', '=', $port)->all();
+  $arrs = Flight::where('ades', '=', $port)->all();
+  $depcount = countflights($deps, 'std');
+  $arrcount = countflights($arrs, 'sta');
+  $count = pivot(array('dep' => $depcount,
+                       'arr' => $arrcount));
+  ksort($count);
+  return $count;
+}
+
+function countflights($flights, $column) {
+  $output = array();
+  foreach ($flights as $flight) {
+    $hour = date('H', strtotime($flight->{$column})) . ":00";
+    if (isset($output[$hour])) {
+      $output[$hour]++;
+    } else {
+      $output[$hour] = 1;
+    }
+  }
+  return $output;
+}
+
+function pivot($tables) {
+  $pivot = array();
+  foreach ($tables as $name => $table) {
+    foreach ($table as $k => $v) {
+      $pivot[$k][$name] = $v;
+    }
+  }
+  foreach ($pivot as $key => $row) {
+    $pivot[$key]['total'] = array_sum($row);
+  }
+  return $pivot;
+}
