f1a43132d371c958c00e2c8712b6041b56da17b7
David Zhong <dzhong111@gmail.com>
2014-07-04 03:42:27 +1000

    Updated ATC info

--- app/controllers/atc/form.php
+++ app/controllers/atc/form.php
@@ -1,6 +1,11 @@
 <?php
+$bookings_open = false;
+
 function _form() {
+  global $bookings_open;
   $data['menu'] = 'atc';
-  if (Auth::check() && check_dupe()) View::output('atc/info', $data);
+  if (Auth::check() && check_dupe() || !$bookings_open) {
+    View::output('atc/info', $data);
+  }
   else View::auto($data);
 }
--- app/views/atc/info.php
+++ app/views/atc/info.php
@@ -5,5 +5,130 @@ if (isset($success)) { ?>
 <?php
 } ?>
 <p class="lead">Thank you for registering to control at RealOps Sydney 2014.</p>
-<p>This page will be updated with the ATC Roster and briefing information closer to the event. You will also receive an email with this information.</p>
 <p>If you are no longer able to attend, please <a href="<?=Route::link("contact")?>">contact us</a> as soon as possible. This will allow us to rearrange the roster to ensure that manpower is best utilized and gaps in ATC coverage are minimized.</p>
+<h3>Late Registration</h3>
+<p>If you haven't already registered and you wish to control at this event, please <a href="<?=Route::link("contact")?>">contact us</a> and we will find you a position.</p>
+<h3>Briefing</h3>
+<p>Please ensure that you read and familiarize yourself with <a href="https://dl.dropboxusercontent.com/u/30534061/vatpac/RealOpsSydney2014.pdf">the ATC briefing</a> and associated documents prior to the event.</p>
+<h3>ATC Roster</h3>
+<table class="table" id="realops-atc-roster">
+  <thead>
+    <tr>
+      <td class="empty"></td>
+      <th>05z-</th>
+      <th>06z-</th>
+      <th>07z-</th>
+      <th>08z-</th>
+      <th>09z-</th>
+      <th>10z-</th>
+      <th>11z-</th>
+      <th>12z-</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th colspan="9">SY TMA</th>
+    </tr>
+    <tr>
+      <th>APP N</th>
+      <td class="empty"></td>
+      <td colspan="2">Mitchell Markovic</td>
+      <td colspan="2">Max Lye</td>
+      <td colspan="2">Peter Story</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>APP S</th>
+      <td class="empty"></td>
+      <td colspan="2">Maurice Ward</td>
+      <td colspan="2">Andrew Harle</td>
+      <td colspan="2">Terry Scanlan</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>DIR W</th>
+      <td class="empty"></td>
+      <td colspan="2">Andrew Harle</td>
+      <td colspan="2" rowspan="2">Mitchell Markovic</td>
+      <td colspan="2" rowspan="2">Rob Grant</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>DIR E</th>
+      <td class="empty"></td>
+      <td colspan="2">Peter Story</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>DEP</th>
+      <td class="empty"></td>
+      <td colspan="2">Callum Strawbridge</td>
+      <td colspan="4">Maurice Ward</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>Flow</th>
+      <td class="empty"></td>
+      <td colspan="6">Kirk Christie</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th colspan="9">SY TWR</th>
+    </tr>
+    <tr>
+      <th>ADC W</th>
+      <td class="empty"></td>
+      <td colspan="2">James Reginato</td>
+      <td colspan="2">Adrian Liu</td>
+      <td colspan="2" rowspan="2">Rowan Liley</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>ADC E</th>
+      <td class="empty"></td>
+      <td colspan="2">Max Lye</td>
+      <td colspan="2">Matt Godwin</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>SMC W</th>
+      <td class="empty"></td>
+      <td colspan="2">Rowan Liley</td>
+      <td colspan="2">Rowan Liley</td>
+      <td colspan="2">Matt Godwin</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th>SMC E</th>
+      <td class="empty"></td>
+      <td colspan="2">Matt Godwin</td>
+      <td colspan="2">Dru Kempler</td>
+      <td colspan="2">Max Lye</td>
+      <td class="empty"></td>
+    </tr>
+    <tr>
+      <th colspan="9">En route</th>
+    </tr>
+    <tr>
+      <th>DOS</th>
+      <td colspan="3" rowspan="2">Kurt D'Amico</td>
+      <td colspan="2" rowspan="2">Callum Strawbridge</td>
+      <td colspan="3" rowspan="2">Aidan Sandri</td>
+    </tr>
+    <tr>
+      <th>ARL</th>
+    </tr>
+    <tr>
+      <th>BIK</th>
+      <td colspan="3">Alex Diederich</td>
+      <td colspan="2" rowspan="2">Alex Diederich</td>
+      <td colspan="3">Mitchell Markovic</td>
+    </tr>
+    <tr>
+      <th>SNO</th>
+      <td colspan="3">Zach Biesse-Fitton</td>
+      <td colspan="3">Andrew Harle</td>
+    </tr>
+  </tbody>
+</table>
+
